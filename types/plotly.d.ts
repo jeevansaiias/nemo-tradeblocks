68ecded29d7944a8c2f3daeccba2c64f162ee33e
Jeevan Sai Simbothula Mukundaiah <nemotaka@Jeevans-MacBook-Pro.local>
2025-11-19 20:31:16 -0500

    chore: add plotly.d.ts to fix build type error

---
+++ types/plotly.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'plotly.js' {
+  export type Data = any;
+  export type Layout = any;
+  export type Config = any;
+  export type PlotData = any;
+}
